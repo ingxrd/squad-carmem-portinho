61a363d815692e0f8e766d53ec6bd35f0054775a
Andiichi <andrezza.n.dasilva@gmail.com>
2024-08-01 19:07:56 -0300

    correcoes ortograficas e limpeza de espaços extras

--- Funcoes_CarmenPortinho/exercicio-01.py
+++ Funcoes_CarmenPortinho/exercicio-01.py
@@ -7,10 +7,10 @@ def soma(resposta): #função que pega a resposta do input e joga para a lista
     return soma_resposta.append(resposta)
 
 
-print('~ Somatorio de numeros ~ ')
-for index in range(5): #faz o loop de até 3 numeros, mas é livre para colocar quantos quiser
-    soma(resposta = int(input(f'Digite o {index+1}º: '))) #chama a função soma() e já tem a entrada do usuario e já adiciona na lista lá na função
+print('~ Somatório de números ~ ')
+for index in range(5): #faz o loop de até 3 números, mas é livre para colocar quantos quiser
+    soma(resposta = int(input(f'Digite o {index+1}º: '))) #chama a função soma() e já tem a entrada do usuário e já adiciona na lista lá na função
 
 
-#mostra a lista com os 3 numeros já somando os numeros.
-print(f'A soma dos numeros é:', sum(soma_resposta))
+#mostra a lista com os 3 números já somando os números.
+print(f'A soma dos números é:', sum(soma_resposta))
