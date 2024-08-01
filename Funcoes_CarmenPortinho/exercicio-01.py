45dddefa68a6edc554d5ad50b6d88b3674a55efa
Andiichi <andrezza.n.dasilva@gmail.com>
2024-08-01 19:06:51 -0300

    melhorias do exercicio 1 do modulo funcoes

--- Funcoes_CarmenPortinho/exercicio-01.py
+++ Funcoes_CarmenPortinho/exercicio-01.py
@@ -1,19 +1,16 @@
 # Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
 
-#iniciando uma lista global vazia
-soma_numeros = []
+#iniciando uma lista vazia
+soma_resposta = []
 
-#função para add os numeros e retorna a soma
-def soma(resposta):    
-    
-    soma_numeros.append(resposta)
-
-    return sum(soma_numeros)
+def soma(resposta): #função que pega a resposta do input e joga para a lista
+    return soma_resposta.append(resposta)
 
 
-#capturando os 3 numeros e enviando para a funcao
-for index in range(3):
-    soma_total = soma(resposta = int(input(f'Digite o  {index + 1}º número: ')))
-    
-#exibindo o resultado
-print('\nA soma dos números é:', soma_total)
+print('~ Somatorio de numeros ~ ')
+for index in range(5): #faz o loop de até 3 numeros, mas é livre para colocar quantos quiser
+    soma(resposta = int(input(f'Digite o {index+1}º: '))) #chama a função soma() e já tem a entrada do usuario e já adiciona na lista lá na função
+
+
+#mostra a lista com os 3 numeros já somando os numeros.
+print(f'A soma dos numeros é:', sum(soma_resposta))
