2067eb15ff3d5fd78e60703465b6eac6c7f3383d
cingrid <devingridmunhoz@gmail.com>
2024-08-02 13:20:43 -0300

    Atualizando o exercicio 5

--- TomadadeDecisao_CarmenPortinho/exercicio-05.py
+++ TomadadeDecisao_CarmenPortinho/exercicio-05.py
@@ -1,11 +1,18 @@
 '''
-Implemente um programa que classifique um aluno com base em sua pontuação em um exame.
-O programa deverá solicitar uma nota de 0 a 10. Se a pontuação for maior ou igual a 7, o aluno é aprovado; 
-caso contrário, é reprovado.
+ Desenvolva um programa que solicite ao usuário os comprimentos dos três
+lados de um triângulo e classifique-o como equilátero, isósceles ou escaleno.
+equilátero: todos os lados com o mesmo valor
+isósceles: dois lados com o mesmo valor
+escaleno: todos os lados com medidas distintas
 '''
 
-notaAluno = float(input("Insira sua pontuação na prova: "))
-if notaAluno >= 7:
-    print("O aluno está aprovado.")
+ladoA = float(input("Insira o Lado A do triangulo: "))
+ladoB = float(input("Insira o Lado B do triangulo: "))
+ladoC = float(input("Insira o Lado C do triangulo: "))
+
+if ladoA == ladoB and ladoB == ladoC:
+    print("Esse é um triângulo equilátero")
+elif ladoA == ladoB or ladoB == ladoC or ladoA == ladoC:
+    print("Esse é um triângulo isósceles")
 else:
-    print("O aluno está reprovado")
+    print("Esse é um triângulo escaleno")
